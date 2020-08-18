48cbee04e53d228d3c572715849c762e76e46c20
Vitor Dullens <vitordullens2.0@gmail.com>
2020-08-18 14:58:35 -0300

    add test GET method in '/info' endpoint

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,7 +1,16 @@
 import request from 'supertest';
 import Server from '../modules/server';
+import Database from '../modules/database/database';
 
 const server = new Server();
+const database = new Database();
+
+test('test if server works', async () => {
+  expect(async () => {
+    await server.startServer();
+    server.closeServer();
+  }).not.toThrow(Error);
+});
 
 it('test GET method in \'/hello\' endpoint', async () => {
   server.startServer();
@@ -14,9 +23,14 @@ it('test GET method in \'/hello\' endpoint', async () => {
   server.closeServer();
 });
 
-test('test if server works', async () => {
-  expect(async () => {
-    await server.startServer();
-    server.closeServer();
-  }).not.toThrow(Error);
+it('test GET method in \'/info\' endpoint', async () => {
+  server.startServer();
+  database.connect();
+
+  const response = await request((await server.getApp()).callback()).get('/info');
+
+  expect(response.status).toBe(200);
+
+  server.closeServer();
+  database.closeConnection();
 });
